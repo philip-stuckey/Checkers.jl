901fc1d95e453c587d0a6ddc32b96dece762977b
colonelkrud <colonelkrud@gmail.com>
2023-07-08 18:42:10 -0400

    Update brute_force.jl

--- src/brute_force.jl
+++ src/brute_force.jl
@@ -1,36 +1,32 @@
 module BruteForce
+
 using Combinatorics
 using CUDA
 using ...CheckersCore: covered
+using ProgressMeter
 
 export brute_force, num_solutions
 
-"""
-    brute_force(n, M=ceil(Int, n * n / 5):(n*n))
-
-    Finds the minimum number of "1"s required to cover a square matrix of side
-    length n. It does this by first looking for any covered boards with 1 "1" on 
-    them, then any board with 2 "1"s, etc... If at any point it finds one, it 
-    returns immediately because it always exhausts boards with fewer "1"s first,
-    therefore the first covered board it finds must be a minimal one.
-"""
 function brute_force(n, M=ceil(Int, n * n / 5):(n*n))
-    d_board = CUDA.zeros(Bool, n, n)  # allocate a GPU array
     h_solution = Vector{Tuple{Array{Bool, 2}, Int}}(undef, 1)  # host array to store solution
 
-    for m in M
+    for (i, m) in enumerate(M)
         combinations = Combinatorics.Combinations(n * n, m)
+        p = Progress(n, 1, "Progress: ", " Solved: $i/$n ", show_percentage = true)
         for combination in combinations
             CUDA.@sync begin
-                d_board .= 0  # Start by clearing the old board
+                d_board = CUDA.zeros(Bool, n, n)  # allocate a GPU array
                 d_board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices
             end
             if CUDA.@sync covered(d_board)
                 h_solution[1] = (CUDA.to_host(copy(d_board)), m)  # if we find any solution, store it in the host array
                 return h_solution[1]  # exit the function early
             end
+            next!(p)  # update the progress bar
         end
+        done!(p)  # mark the progress bar as done
     end
+
     return (Bool[;;], 0)  # if the function gets this far, presumably no solutions exist
 end
 
