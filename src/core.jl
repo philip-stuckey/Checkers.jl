fcc02da8a37a9c1de49e463934a5e3a726c46270
colonelkrud <colonelkrud@gmail.com>
2023-07-08 18:45:30 -0400

    Update core.jl
    create kernels for neighbors and covered. Add descriptions for functions. Make returns CUDA friendly.

--- src/core.jl
+++ src/core.jl
@@ -1,85 +1,89 @@
 module CheckersCore
+    using CUDA
 
-using CUDA
+    export neighbors, covered
 
-export Pad, getindex, eltype
-export covered, neighbors
+    """
+    neighbors_kernel!(result, arr, nrows, ncols)
 
-"""
-I didn't want to deal with literal edge cases, so I made this wrapper struct
-to return 0 when you try to index outside of the bounds of the matrix.
-"""
-struct Pad{T,A<:AbstractMatrix{T}}
-    arr::A
-    fill::T
-end
+    CUDA kernel function that calculates the number of neighboring cells with a value of `true`
+    for each cell in the input array `arr`.
 
-Pad(arr::AbstractMatrix{T}) where {T} = Pad(arr, zero(T))
-
-function Base.getindex(z::Pad{T}, i, j)::T where {T}
-index = CartesianIndex(i, j)
-indices = CartesianIndices(z.arr)::CartesianIndices{2,Tuple{Base.OneTo{Int64},Base.OneTo{Int64}}}
-return (index in indices) ? z.arr[i, j] : z.fill
-end
-
-Base.getindex(z::Pad, c::CartesianIndex{2}) = getindex(z, Tuple(c)...)
-
-"""
-this function is helpful for type stability. Essentially it informs the
-compiler that indexing a ZeroPad object will return the same type as the
-underlying matrix.
-"""
-Base.eltype(::Type{Pad{T}}) where {T} = T
-
-"""
-get the Von Neumann neighborhood (of radius 1) around the (i, j)th element of
-matrix x. Here is where we use Pad to avoid having to worry about indices
-out of bounds.
-"""
-function neighbors(z::Pad, (i, j))
-neighbors = CUDA.zeros(Int8, 5)
-neighbors[1] = z[i-1, j]
-neighbors[2] = z[i, j-1]
-neighbors[3] = z[i, j+1]
-neighbors[4] = z[i+1, j]
-neighbors[5] = z[i, j]
-return neighbors
-end
-
-function neighbors(z::Pad, I::CartesianIndex{2})
-return neighbors(z, Tuple(I))
-end
-
-function neighbors(m::AbstractMatrix{T}, I; fill=zero(T)) where {T}
-return neighbors(Pad(m, fill), I)
-end
-
-function neighbors(z::Pad)
-    result = CUDA.zeros(Int, size(z.arr, 1) * size(z.arr, 2))
-     function foo()
+    Args:
+    - `result`: The output array to store the computed neighbor counts.
+    - `arr`: The input boolean array representing the game board.
+    - `nrows`: The number of rows in the array.
+    - `ncols`: The number of columns in the array.
+    """
+    function neighbors_kernel!(result, arr, nrows, ncols)
         idx = (blockIdx().x - 1) * blockDim().x + threadIdx().x
         if idx <= length(result)
-            i, j = (idx - 1) ÷ size(z.arr, 2) + 1, (idx - 1) % size(z.arr, 2) + 1
-            result[idx] = sum(neighbors(z, (i, j)))
+            i, j = (idx - 1) ÷ ncols + 1, (idx - 1) % ncols + 1
+            count = 0
+            for di = max(i-1, 1):min(i+1, nrows)
+                for dj = max(j-1, 1):min(j+1, ncols)
+                    @inbounds count += arr[di, dj]
+                end
+            end
+            result[idx] = count
         end
         return nothing
     end
-    @cuda threads=length(result) blocks=1 foo()
-    return result
-end
 
-function neighbors(m::AbstractMatrix{T}; fill=zero(T)) where {T}
-return neighbors(Pad(m, fill))
-end
+    """
+    neighbors(arr::CUDA.CuArray{Bool})
 
-"""
-A matrix is "covered" if every element either is 1 or has a 1 in its
-neighborhood.
-"""
-function covered(A)
-return all(A .== 1 .| neighbors(A))
-end
+    Calculates the number of neighboring cells with a value of `true`
+    for each cell in the input array `arr` using GPU acceleration.
 
-@warn "Lazy programmer warning: exporting all symbols"
+    Args:
+    - `arr`: The input boolean array representing the game board.
 
+    Returns:
+    - An integer array representing the number of neighbors for each cell.
+    """
+    function neighbors(arr::CUDA.CuArray{Bool})
+        nrows, ncols = size(arr, 1), size(arr, 2)
+        result = CUDA.zeros(Int, nrows * ncols)
+        @cuda threads=ceil(Int, (nrows*ncols)/1024) neighbors_kernel!(result, arr, nrows, ncols)
+        return result
+    end
+
+    """
+    covered(A::CUDA.CuArray{Bool})
+
+    Checks if all cells in the input array `A` are covered.
+
+    Args:
+    - `A`: The input boolean array representing the game board.
+
+    Returns:
+    - `true` if all cells are covered, `false` otherwise.
+    """
+    function covered(A::CUDA.CuArray{Bool})
+        nrows, ncols = size(A, 1), size(A, 2)
+        result = CUDA.zeros(Bool, nrows, ncols)
+        @cuda threads=ceil(Int, (nrows*ncols)/1024) covered_kernel!(result, A, nrows, ncols)
+        return all(result)
+    end
+
+    """
+    covered_kernel!(result, arr, nrows, ncols)
+
+    CUDA kernel function that checks if each cell in the input array `arr` is covered.
+
+    Args:
+    - `result`: The output boolean array to store the coverage status.
+    - `arr`: The input boolean array representing the game board.
+    - `nrows`: The number of rows in the array.
+    - `ncols`: The number of columns in the array.
+    """
+    function covered_kernel!(result, arr, nrows, ncols)
+        idx = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+        if idx <= nrows * ncols
+            i, j = (idx - 1) ÷ ncols + 1, (idx - 1) % ncols + 1
+            result[idx] = arr[i, j] == 1
+        end
+        return nothing
+    end
 end
