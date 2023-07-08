baf446145b33dcca02b4c5c35b6b69ee176eba96
colonelkrud <colonelkrud@gmail.com>
2023-07-08 00:59:53 -0400

    Update brute_force.jl
    Make it CUDA

--- src/brute_force.jl
+++ src/brute_force.jl
@@ -1,29 +1,33 @@
 module BruteForce
 using Combinatorics
+using CUDA
 using ...CheckersCore: covered
 
 export brute_force, num_solutions
 
 """
-    brute_foece(n, M=ceil(Int, n * n / 5):(n*n))
+    brute_force(n, M=ceil(Int, n * n / 5):(n*n))
 
-	Finds the minimum number of "1"s required to cover a square matrix of side
-length n. It does this by first looking for any covered boards with 1 "1" on 
-them, then any board with 2 "1"s, etc... If at any point it finds one, it 
-returns immediatly, because it always exhausts boards with fewer "1"s first,
-therefore the first covered board it finds must be a minimal one.
+    Finds the minimum number of "1"s required to cover a square matrix of side
+    length n. It does this by first looking for any covered boards with 1 "1" on 
+    them, then any board with 2 "1"s, etc... If at any point it finds one, it 
+    returns immediately because it always exhausts boards with fewer "1"s first,
+    therefore the first covered board it finds must be a minimal one.
 """
 function brute_force(n, M=ceil(Int, n * n / 5):(n*n))
-    board = zeros(Bool, n, n)  # pre-allocate a board
+    d_board = CUDA.zeros(Bool, n, n)  # allocate a GPU array
+    h_solution = Vector{Tuple{Array{Bool, 2}, Int}}(undef, 1)  # host array to store solution
+
     for m in M
-        # make an iterator over every combination of indices length m. These is where we'll put the 1s
         combinations = Combinatorics.Combinations(n * n, m)
-        #@info "$(now())" m  log10(length(combinations))
-        for combination in combinations  # most of the allocations happen here
-            board .= 0  # Start by clearing the old board, this doesn't allocate
-            board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices. This allocates a little bit
-            if covered(board)  # check if the board is covered. This doesn't allocate somehow
-                return (board, m)  # if we find any solution, return early 
+        for combination in combinations
+            CUDA.@sync begin
+                d_board .= 0  # Start by clearing the old board
+                d_board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices
+            end
+            if CUDA.@sync covered(d_board)
+                h_solution[1] = (CUDA.to_host(copy(d_board)), m)  # if we find any solution, store it in the host array
+                return h_solution[1]  # exit the function early
             end
         end
     end
@@ -31,19 +35,21 @@ function brute_force(n, M=ceil(Int, n * n / 5):(n*n))
 end
 
 function num_solutions(n, m)
-    board = zeros(Bool, n, n)  # pre-allocate a board
-    result = 0
-    # make an iterator over every combination of indices length m. These is where we'll put the 1s
+    d_board = CUDA.zeros(Bool, n, n)  # allocate a GPU array
+    result = CUDA.zeros(Int)
     combinations = Combinatorics.Combinations(n * n, m)
-    #@info "$(now())" m  log10(length(combinations))
-    for combination in combinations  # most of the allocations happen here
-        board .= 0  # Start by clearing the old board, this doesn't allocate
-        board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices. This allocates a little bit
-        if covered(board)  # check if the board is covered. This doesn't allocate somehow
-            result += 1
+
+    for combination in combinations
+        CUDA.@sync begin
+            d_board .= 0  # Start by clearing the old board
+            d_board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices
+        end
+        if CUDA.@sync covered(d_board)
+            @cuda atomic add!(result, 1)
         end
     end
-    return result
+
+    return CUDA.to_host(result[1])
 end
 
 end
