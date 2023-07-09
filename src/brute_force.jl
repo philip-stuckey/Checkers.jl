a153d58be774bcc3bdfad6ab8284c5cd4bec6682
colonelkrud <colonelkrud@gmail.com>
2023-07-08 23:33:44 -0400

    Update brute_force.jl
    +num_solutions
    +Progress bar and metrics again
    collect combinations into array
    calculate total combinations for an estimation of metrics

--- src/brute_force.jl
+++ src/brute_force.jl
@@ -24,9 +24,11 @@ Returns:
 """
 function brute_force(n, M=ceil(Int, n * n / 5):(n*n))
     h_solution = Vector{Tuple{Array{Bool, 2}, Int}}(undef, 1)  # host array to store solution
+    total_combinations = length(collect(Combinatorics.Combinations(n * n, maximum(M))))
+    p = Progress(total_combinations, 1, "Progress: ", " Combinations: ")
 
     for (i, m) in enumerate(M)
-        combinations = Combinatorics.Combinations(n * n, m)
+        combinations = collect(Combinatorics.Combinations(n * n, m))  # Convert Combinations object to array
         num_tried = 0
         start_time = now()
         for combination in combinations
@@ -37,13 +39,56 @@ function brute_force(n, M=ceil(Int, n * n / 5):(n*n))
             num_tried += 1
             if CUDA.@sync covered(d_board)
                 h_solution[1] = (CUDA.to_host(copy(d_board)), m)  # if we find any solution, store it in the host array
+                elapsed_time = Dates.now() - start_time
+                iterations_per_second = num_tried / Dates.value(Dates.Millisecond(elapsed_time))
+                println("Solution found!")
+                println("Combination: ", combination)
+                println("Combinations tried: ", num_tried, "/", total_combinations)
+                println("Iterations per second: ", iterations_per_second)
                 return h_solution[1]  # exit the function early
             end
+            next!(p)  # update the progress bar
         end
+        done!(p)  # mark the progress bar as done
     end
 
-    return (Bool[;;], 0)  # if the function gets this far, presumably no solutions exist
+    # If no solution is found, return an empty boolean array
+    return (Bool[;;], 0)
 end
 
+"""
+num_solutions(n, M=ceil(Int, n * n / 5):(n*n))
+
+Counts the number of solutions for the checkers problem.
+
+Args:
+- `n`: The size of the checkers board (n x n).
+- `M`: The range of task sizes to consider (default: 1/5 to n^2).
+
+Returns:
+- The number of solutions found.
+"""
+function num_solutions(n, M=ceil(Int, n * n / 5):(n*n))
+    num_solutions = 0
+    total_combinations = length(collect(Combinatorics.Combinations(n * n, maximum(M))))
+    p = Progress(total_combinations, 1, "Progress: ", " Combinations: ")
+
+    for (i, m) in enumerate(M)
+        combinations = collect(Combinatorics.Combinations(n * n, m))  # Convert Combinations object to array
+        for combination in combinations
+            CUDA.@sync begin
+                d_board = CUDA.zeros(Bool, n, n)  # allocate a GPU array
+                d_board[combination] .= 1  # put a 1 on every part of the board specified by that combination of indices
+            end
+            if CUDA.@sync covered(d_board)
+                num_solutions += 1
+            end
+            next!(p)  # update the progress bar
+        end
+        done!(p)  # mark the progress bar as done
+    end
+
+    return num_solutions
+end
 
 end
