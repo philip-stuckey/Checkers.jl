9dc418268796ae1186216153c1fc5821be0221df
colonelkrud <colonelkrud@gmail.com>
2023-07-08 23:30:56 -0400

    Update tests.jl
    better testing - currently has a bug with converting string to int

--- src/tests.jl
+++ src/tests.jl
@@ -1,23 +1,32 @@
 using Test
+include("core.jl")
 include("brute_force.jl")
 using .BruteForce
 
-function test_valid_solution()
-	@test_throws MethodError valid_solution(-1, [true], (1,))
-	@test_throws MethodError valid_solution(0, [true], (1,))
-  @test valid_solution(1, [true], (1,))
-  @test valid_solution(2, [false true; false false], (1,))
-  @test valid_solution(2, [true false; false false], (2,))
-end
-
+# Function to validate the solution obtained from the brute_force function
 function valid_solution(n, solution, expected)
-    result = brute_force(n)
-    @test result[1] == solution
-    @test result[2] in expected
+    # Check if the obtained solution matches the expected solution
+    board, m = brute_force(n)
+    @test board == solution
+
+    # Check if the correct error is thrown when using invalid arguments for brute_force and valid_solution functions
+    @test_throws UndefVarError BruteForce.brute_force(1, 1:1)
+    @test_throws MethodError valid_solution(1, [true], (1,))
+
+    # Check if the valid_solution function returns the expected result for the given solution
+    @test valid_solution(n, solution) == expected
 end
 
+# Test set for the brute_force function
 @testset "Brute Force Algorithm" begin
-    @testset "Valid Solution" begin
-        test_valid_solution()
-    end
+    @test brute_force(1) == ([true], 1)  # Test for 1x1 board
+    @test brute_force(2) == ([true false; true false], 2)  # Test for 2x2 board
+    @test brute_force(3) == ([true false false; false false true; true false false], 3)  # Test for 3x3 board
+end
+
+# Test set for the valid_solution function
+@testset "Valid Solution" begin
+    valid_solution(1, [true], ([true], 1))  # Test for 1x1 board with expected solution
+    valid_solution(2, [true false; true false], ([true false; true false], 2))  # Test for 2x2 board with expected solution
+    valid_solution(3, [true false false; false false true; true false false], ([true false false; false false true; true false false], 3))  # Test for 3x3 board with expected solution
 end
